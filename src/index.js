16b1832a778a292ee214238c2d4f480bc4a8b5ab
Tanyaa-a <120506794+Tanyaa-a@users.noreply.github.com>
2023-02-27 12:32:51 -0800

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  
+  let bracketsMap = new Map(bracketsConfig);
+
+  for (let i = 0; i < str.length; i++) {
+    let currentBracket = str[i];
+    let lastBracket = stack[stack.length - 1];
+    
+    if (bracketsMap.has(currentBracket) && bracketsMap.get(currentBracket) === lastBracket) {
+      stack.pop();
+    } else if (bracketsMap.has(currentBracket)) {
+      stack.push(currentBracket);
+    } else if (currentBracket === bracketsMap.get(lastBracket)) {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+  
+  return stack.length === 0;
 }
